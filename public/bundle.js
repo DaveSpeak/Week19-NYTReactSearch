c943d53e1026187f628a8eca5c39fd338f0c7ad9
DaveSpeak <DaveSpeak@gmail.com>
2016-11-12 00:04:16 -0800

    fixing heroku deployment

--- public/bundle.js
+++ public/bundle.js
@@ -20159,8 +20159,8 @@
 		// This function serves our purpose of running the query to geolocate. 
 		runQuery: function runQuery(location, startYear, endYear) {
 
-			//Figure out the geolocation
-			var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + nytAPI + "&q=" + location + "&begin_date=" + startYear + "0101" + "&end_date=" + endYear + "0101";
+			//Call the query
+			var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + nytAPI + "&q=" + location + "&begin_date=" + startYear + "0101" + "&end_date=" + endYear + "0101";
 
 			return axios.get(queryURL).then(function (response) {
 
