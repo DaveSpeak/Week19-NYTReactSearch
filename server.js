6b7404c7fdd61142f9187638a835d5fc96984a03
DaveSpeak <DaveSpeak@gmail.com>
2016-11-11 23:33:16 -0800

    update for heroku deployment

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static('./public'));
 // -------------------------------------------------
 
 // MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect('mongodb://localhost');
+mongoose.connect('mongodb://mongodb://heroku_744n9qqt:3mqnoliqp6netto2vsipvckf7h@ds151707.mlab.com:51707/heroku_744n9qqt');
 var db = mongoose.connection;
 
 db.on('error', function (err) {
